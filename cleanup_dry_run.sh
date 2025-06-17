8d548590edbe968c4b7f1eee2affbc07b52d7c93
ientebib <153154591+ientebib@users.noreply.github.com>
2025-06-17 11:18:24 -0600

    Remove obsolete dashboard and debug files

--- cleanup_dry_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Dry run version of cleanup script
-echo "This is a dry run - nothing will be deleted"
-echo "Old marker files that would be removed:"
-find . -name "redshift_queried_*.marker" | grep -v "$(date +%Y%m%d)"
--- start_modern_dashboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Open backend in new Terminal tab
-osascript -e 'tell application "Terminal" to do script "cd \"$(pwd)/modern_dashboard/backend\"; python3 main.py"'
-
-# Open frontend in new Terminal tab
-osascript -e 'tell application "Terminal" to do script "cd \"$(pwd)/modern_dashboard/frontend\"; npm install; npm run dev"' 
